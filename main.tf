07ea383f000d3fd597358a9ff9210a27b6ec3f12
zeehashmi82 <zee_hashmi2002@yahoo.com>
2024-05-03 19:50:19 -0400

    test11

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 
 module "ec2_instance" {
-  source              = "./module/ec2_instance"
+  source              = "./modules/ec2_instance"
   ami_value           = "ami-07caf09b362be10b8"
   instance_type_value = "t2.micro"
   subnet_id_value     = "subnet-08666ec4cfef45636"
